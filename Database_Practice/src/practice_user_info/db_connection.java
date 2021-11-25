24811aa48c79b38dfbf6a042505ef19586338fe8
김성호 <kksshh0612@gmail.com>
2021-11-26 00:28:04 +0900

    디비 연동

--- Database_Practice/src/practice_user_info/db_connection.java
+++ Database_Practice/src/practice_user_info/db_connection.java
@@ -2,13 +2,7 @@ package practice_user_info;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
-
-//import com.mysql.cj.protocol.Resultset;
-//import com.mysql.cj.xdevapi.Statement;
 import java.sql.Statement;
-
-import com.mysql.cj.util.DnsSrv.SrvRecord;
-
 import java.sql.ResultSet;
 
 public class db_connection {
@@ -28,17 +22,37 @@ public class db_connection {
 		}
 	}
 	
-	public boolean is_info(String id, String pwd) {
+	//회원가입에서 정보 넘겨받아 데이터베이스(sinamon의 user_info 테이블)에 저장하는 함수
+	public boolean input_user_info(String name, String nickname, String id, String pwd, String home) {		
 		try {
-			String SQL = "SELECT * FROM user_info WHERE user_id = " + id + "and password = " + pwd;
-			rs = st.executeQuery(SQL);
-			if(rs.next()) {
-				System.out.println(rs.getString(4));
-				System.out.println(rs.getString(5));
+			if(is_info_exist(id)) {		//id값과 존재하는 user_id가 있을 때	
+				System.out.println("이미 존재하는 계정입니다.");
+				return false;
+			}
+			else {		//아이디가 존재하지 않으면 user_info 입력
+				String SQL = "INSERT INTO user_info(name, nickname, user_id, password, home) VALUES("+ name +"," + nickname + "," + id + "," + pwd + "," + home + ");";    
+				int record_num = st.executeUpdate(SQL);		//INSERT 쿼리는 executeUpdate를 사용한다. 이것은 업데이트된 건수를 int로 리턴한다.
+				System.out.println("업데이트된 건 수: " + record_num);
 			}
 		} 
 		catch (Exception e) {
-			System.out.println("데이터베이스 검색 오류: " + e.getMessage());
+			System.out.println("데이터베이스 입력 오류/ Code: " + e.getMessage());
+		}
+		return true;
+	}
+	
+	public boolean is_info_exist(String id) {		//로그인 했을때 아이디 비번이 일치하는지 확인
+		try {
+			String SQL = "SELECT * FROM user_info WHERE user_id = " + id;
+			rs = st.executeQuery(SQL);		//SELECT 구문에 사용
+			if(rs.next()) {
+				System.out.println(rs.getString(4));
+				System.out.println(rs.getString(5));
+				return true;
+			}	
+		} 
+		catch (Exception e) {
+			System.out.println("데이터베이스 검색 오류/ Code: " + e.getMessage());
 		}
 		return false;	//검색되지 않았다면 false 반환
 	}
--- Database_Practice/src/practice_user_info/user_info.java
+++ Database_Practice/src/practice_user_info/user_info.java
@@ -4,7 +4,8 @@ public class user_info {
 
 	public static void main(String[] args) {
 		db_connection connection = new db_connection();
-		System.out.println("관리자 여부: " + connection.is_info("'kksshh0612'", "'kksshh1735'"));
+		//System.out.println("관리자 여부: " + connection.is_info_exist("'kksshh0612'"));
+		connection.input_user_info("'서민정'", "'서민'", "'smj'", "'0228'", "'서문'");
 	}
 
 }
